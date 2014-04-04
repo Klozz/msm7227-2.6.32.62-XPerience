cc292d5469246fb97040fa2b6873faf91c9949f9
klozz (TeamMEX) <xxx.reptar.rawrr.xxx@gmail.com>
2014-04-03 20:28:07 -0600

    Updated msm-rotator.

--- include/linux/msm_rotator.h
+++ include/linux/msm_rotator.h
@@ -28,6 +28,7 @@
  */
 
 #ifndef __MSM_ROTATOR_H__
+#define __MSM_ROTATOR_H__
 
 #include <linux/types.h>
 #include <linux/msm_mdp.h>
@@ -41,8 +42,10 @@
 #define MSM_ROTATOR_IOCTL_FINISH   \
 		_IOW(MSM_ROTATOR_IOCTL_MAGIC, 3, int)
 
+#define ROTATOR_VERSION_01	0xA5B4C301
+
 enum rotator_clk_type {
-	ROTATOR_AXI_CLK,
+	ROTATOR_CORE_CLK,
 	ROTATOR_PCLK,
 	ROTATOR_IMEM_CLK
 };
@@ -56,12 +59,16 @@ struct msm_rotator_img_info {
 	unsigned int    dst_y;
 	unsigned char   rotations;
 	int enable;
+	unsigned int downscale_ratio;
 };
 
 struct msm_rotator_data_info {
 	int session_id;
 	struct msmfb_data src;
 	struct msmfb_data dst;
+	unsigned int version_key;
+	struct msmfb_data src_chroma;
+	struct msmfb_data dst_chroma;
 };
 
 struct msm_rot_clocks {
@@ -74,6 +81,9 @@ struct msm_rotator_platform_data {
 	unsigned int number_of_clocks;
 	unsigned int hardware_version_number;
 	struct msm_rot_clocks *rotator_clks;
+	const char *regulator_name;
+#ifdef CONFIG_MSM_BUS_SCALING
+	struct msm_bus_scale_pdata *bus_scale_table;
+#endif
 };
 #endif
-
