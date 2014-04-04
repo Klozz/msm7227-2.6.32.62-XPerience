311814418ae779a546cf44e820016adcff6f978e
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-04-04 17:15:45 -0600

    Deleted duplicated files x_x

--- include/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 6 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 PREEMPT Fri Apr 4 06:02:07 CST 2014"
-#define LINUX_COMPILE_TIME "06:02:07"
-#define LINUX_COMPILE_BY "klozz"
-#define LINUX_COMPILE_HOST "Mizaki-MEI"
-#define LINUX_COMPILE_DOMAIN
-#define LINUX_COMPILER "gcc version 4.7.4 20140107 (prerelease) (crosstool-NG linaro-1.13.1+bzr2644 - Linaro GCC 2014.01) "
